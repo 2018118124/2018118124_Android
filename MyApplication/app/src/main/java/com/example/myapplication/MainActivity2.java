778423ce822176952bd173f7fe2723d2e83aa362
KOTIN\Administrator <1019268193@qq.com>
2020-09-21 10:57:44 +0800

    第一次作业最终提交

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,72 +3,76 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 public class MainActivity2 extends AppCompatActivity {
-        static int m=0;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    private static final String TAG = "ManActivity2" ;
+    static int m=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-            super.onCreate(savedInstanceState);
-            m=m+1;
-            System.out.println("创建活动   "+m);
-            setContentView(R.layout.activity_main2);
-            Button button=(Button)findViewById(R.id.button_1);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(MainActivity2.this,"点击按钮",Toast.LENGTH_LONG).show();
-                }
-            });
-        }
+        super.onCreate(savedInstanceState);
 
-        @Override
-        protected void onStart() {
-
-
-            super.onStart();
-            m=m+1;
-            System.out.println("启动"+m);
-        }
-
-        @Override
-        protected void onPause() {
-
-            super.onPause();
-            m=m+1;
-            System.out.println("暂停活动回调 "+m);
-        }
-
-        @Override
-        protected void onResume() {
-
-            m=m+1;
-            super.onResume();
-            System.out.println("恢复活动回调"+m);
-        }
-
-        @Override
-        protected void onStop() {
-
-            super.onStop();
-            m=m+1;
-            System.out.println("停止活动回调 "+m);
-        }
-
-        @Override
-        protected void onDestroy() {
-
-            super.onDestroy();
-            m=m+1;
-            System.out.println("销毁活动回调 "+m);
-        }
-        @Override
-        protected  void onRestart() {
-
-            super.onRestart();
-            m=m+1;
-            System.out.println("重启活动回调 "+m);
-        }
+        Log.d(TAG,"onCreate");
+        setContentView(R.layout.activity_main2);
+        Button button=(Button)findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity2.this,"点击按钮",Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+
+
+        super.onStart();
+        Log.d(TAG,"onStart");
+
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        Log.d(TAG,"onPause");
+
+    }
+
+    @Override
+    protected void onResume() {
+
+
+        super.onResume();
+        Log.d(TAG,"onResume");
+
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+        Log.d(TAG,"onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+
+    }
+    @Override
+    protected  void onRestart() {
+
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+
+    }
+}
