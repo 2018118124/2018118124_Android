0b07eeaef88562d762c8e7e447f0be11d2839f79
KOTIN\Administrator <1019268193@qq.com>
2020-09-27 21:48:04 +0800

    使用显式Intent

--- Application02/app/src/main/java/com/example/application02/FirstActivity.java
+++ Application02/app/src/main/java/com/example/application02/FirstActivity.java
@@ -2,6 +2,7 @@ package com.example.application02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,7 +21,8 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this, "You clicked Button 1",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
             }
         });
     }
