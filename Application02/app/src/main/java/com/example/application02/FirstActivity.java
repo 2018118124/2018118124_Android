c03b60e8f4b240bf344e92eebdd5212ae76f1656
KOTIN\Administrator <1019268193@qq.com>
2020-09-28 21:28:52 +0800

    standard模式

--- Application02/app/src/main/java/com/example/application02/FirstActivity.java
+++ Application02/app/src/main/java/com/example/application02/FirstActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,16 +16,26 @@ import android.widget.Toast;
 public class FirstActivity extends AppCompatActivity {
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch (requestCode){
+            case 1:
+                if (resultCode == RESULT_OK){
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("FirstActivity", this.toString());
         setContentView(R.layout.first_layout);
         Button button1 =(Button)findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String data ="Hello SecondActivity";
                 Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
-                intent.putExtra("extra_data", data);
                 startActivity(intent);
             }
         });
